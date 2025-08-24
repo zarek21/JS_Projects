d6f9bc139a2dde34bf48ccbd3e7b624b624adc20
Jorge <jorge.saleme77@gmail.com>
2025-08-24 14:29:26 -0600

    implement void text field advertise

--- Guess_my_number/starter/script.js
+++ Guess_my_number/starter/script.js
@@ -17,5 +17,10 @@
 // document.querySelector('.check').addEventListener('click',x);
 
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🛑 No number!';
+  }
 });
