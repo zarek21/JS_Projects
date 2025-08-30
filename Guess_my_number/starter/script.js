d1b761fe41a08f9f75c05942450e7ecb324c41dd
Zarek Saleme <jorge.saleme77@gmail.com>
2025-08-30 13:39:59 -0600

    :sparkles: add more interaction

--- Guess_my_number/starter/script.js
+++ Guess_my_number/starter/script.js
@@ -16,11 +16,20 @@
 
 // document.querySelector('.check').addEventListener('click',x);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '🛑 No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '😒 Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '😒 Too low!';
   }
 });
