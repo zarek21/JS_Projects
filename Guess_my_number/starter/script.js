a7c6616f375480310348005398add87611490405
Zarek Saleme <jorge.saleme77@gmail.com>
2025-08-30 15:02:17 -0600

    :sparkles: finish game

--- Guess_my_number/starter/script.js
+++ Guess_my_number/starter/script.js
@@ -18,6 +18,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -35,6 +36,12 @@ document.querySelector('.check').addEventListener('click', function () {
     // MANIPULATE CSS STYLES
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    // HIGHSCORE CHECK
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
   // When guess is too high
   else if (guess > secretNumber) {
