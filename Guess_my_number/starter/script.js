9989a943b4a2ad8b6717007c3fdc638c11b39e58
Zarek Saleme <jorge.saleme77@gmail.com>
2025-08-30 13:58:36 -0600

    :sparkles: finish game logic

--- Guess_my_number/starter/script.js
+++ Guess_my_number/starter/script.js
@@ -29,12 +29,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '😒 Too high!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '😒 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🤷‍♂️ You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '😒 Too low!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '😒 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🤷‍♂️ You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
