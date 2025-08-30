b187091814cd22ec4efc44d4a86faeb650834c2f
Zarek Saleme <jorge.saleme77@gmail.com>
2025-08-30 14:21:01 -0600

    :sparkles: manipulate styles

--- Guess_my_number/starter/script.js
+++ Guess_my_number/starter/script.js
@@ -24,11 +24,19 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '🛑 No number!';
-  } else if (guess === secretNumber) {
+  }
+  // When player wins
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-  } else if (guess > secretNumber) {
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+  }
+  // When guess is too high
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '😒 Too high!';
       score--;
@@ -37,7 +45,9 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🤷‍♂️ You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  }
+  // When guess is too low
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '😒 Too low!';
       score--;
